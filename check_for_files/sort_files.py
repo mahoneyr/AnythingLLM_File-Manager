28692e51f4dff7e499eb1fa0f2069c4eac3adca8
Ron Metzger <ron.metzger@korn-recycling.de>
2025-05-19 08:22:07 +0200

    fix: Issue#1 - loading URL from .env file instead of default wrong URL

--- check_for_files/sort_files.py
+++ check_for_files/sort_files.py
@@ -1,22 +1,30 @@
 import requests
 import urllib3
+import os
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class SortFiles:
     def __init__(self, verbose=False):
+        self.AnythingLLM_api = os.environ.get("ANYTHING_LLM_API")
+        if not self.AnythingLLM_api:
+            raise ValueError("ANYTHING_LLM_API environment variable is not set")
         self.headers_json = {  #Header for most request types
             "accept": "application/json",
-            "Authorization": f"Bearer XHPMBWW-20VMQRB-NS2D1GQ-BE3246J",
+            "Authorization": f"Bearer {self.AnythingLLM_api}",
             "Content-Type": "application/json",
         }
         self.verbose = verbose
         self.embedded_documents = {}
+        
+        self.main_url = os.environ.get("ANYTHING_LLM_URL")
+        if not self.main_url:
+            raise ValueError("ANYTHING_LLM_URL environment variable is not set")
 
     def reset(self) -> None:
         self.embedded_documents = {}
 
     def get_folders_list(self) -> list[str]:
-        response = requests.get("https://kichat.korn.local:3003/api/v1/documents", headers=self.headers_json, timeout=10, verify=False).json()
+        response = requests.get(f"{self.main_url}/api/v1/documents", headers=self.headers_json, timeout=10, verify=False).json()
         localFiles = response["localFiles"]
         folders = localFiles["items"]
         all_folders = []
@@ -31,7 +39,7 @@ class SortFiles:
                         "deletes": delete
                     }
         if self.verbose: print(f"json_to_send embedding: {json_to_send}")
-        response = requests.post(f"https://kichat.korn.local:3003/api/v1/workspace/{workspace}/update-embeddings", headers=self.headers_json, timeout=10, verify=False, json=json_to_send)
+        response = requests.post(f"{self.main_url}/api/v1/workspace/{workspace}/update-embeddings", headers=self.headers_json, timeout=10, verify=False, json=json_to_send)
 
     def move_document(self, docpath, docname, folder_name) -> str:
         new_path = folder_name + "/" + docname
@@ -44,15 +52,15 @@ class SortFiles:
             ]
         }
         if self.verbose: print(f"json_to_send: {json_to_send}")
-        response = requests.post(f"https://kichat.korn.local:3003/api/v1/document/move-files", headers=self.headers_json, timeout=10, verify=False, json=json_to_send)
+        response = requests.post(f"{self.main_url}/api/v1/document/move-files", headers=self.headers_json, timeout=10, verify=False, json=json_to_send)
         return new_path
 
     def sort_files(self) -> None:
         # step 1: get all embedded documents
-        response = requests.get("https://kichat.korn.local:3003/api/v1/workspaces", headers=self.headers_json, timeout=10, verify=False).json()
+        response = requests.get(f"{self.main_url}/api/v1/workspaces", headers=self.headers_json, timeout=10, verify=False).json()
         workspaces = response["workspaces"]
         for workspace in workspaces:
-            response = requests.get(f"https://kichat.korn.local:3003/api/v1/workspace/{workspace['slug']}", headers=self.headers_json, timeout=10, verify=False).json()
+            response = requests.get(f"{self.main_url}/api/v1/workspace/{workspace['slug']}", headers=self.headers_json, timeout=10, verify=False).json()
             workspace = response["workspace"][0]
             documents = workspace["documents"]
             for doc in documents:
@@ -81,7 +89,7 @@ class SortFiles:
                 if self.verbose:print(f"new foldername: {new_foldername}")
 
                 if new_foldername not in all_folders:
-                    response = requests.post("https://kichat.korn.local:3003/api/v1/document/create-folder", headers=self.headers_json, timeout=10, verify=False, json={"name": new_foldername}).json()
+                    response = requests.post(f"{self.main_url}/api/v1/document/create-folder", headers=self.headers_json, timeout=10, verify=False, json={"name": new_foldername}).json()
                 new_path = self.move_document(self.embedded_documents[doc]["path"], doc, new_foldername)
                 self.change_embedding(self.embedded_documents[doc]["in_workspaces"][0], add=[new_path], delete=[])
                 files_moved += 1
